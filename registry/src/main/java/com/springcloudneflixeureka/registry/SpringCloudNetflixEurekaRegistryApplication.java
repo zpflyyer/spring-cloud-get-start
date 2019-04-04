1dbd9c18bb076bf0c33528a5b07a6df2fc82c951
zpfly <15710165386@163.com>
2019-04-04 15:44:36 +0800

    【Eureka】成功配置registry并注册service1

--- registry/src/main/java/com/springcloudneflixeureka/registry/SpringCloudNetflixEurekaRegistryApplication.java
+++ registry/src/main/java/com/springcloudneflixeureka/registry/SpringCloudNetflixEurekaRegistryApplication.java
@@ -1,4 +1,4 @@
-package springcloudlearn.springcloudnetflixeureka;
+package com.springcloudneflixeureka.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class SpringCloudNetflixEurekaApplication {
+public class SpringCloudNetflixEurekaRegistryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudNetflixEurekaApplication.class, args);
+		SpringApplication.run(SpringCloudNetflixEurekaRegistryApplication.class, args);
 	}
 
 }
--- registry/src/test/java/com/springcloudneflixeureka/registry/SpringCloudNetflixEurekaApplicationTests.java
+++ registry/src/test/java/com/springcloudneflixeureka/registry/SpringCloudNetflixEurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package springcloudlearn.springcloudnetflixeureka;
+package com.springcloudneflixeureka.registry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
