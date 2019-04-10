79774e1e5d0c5f83dd461a21d5c0b6dac603ae8e
zpfly <15710165386@163.com>
2019-04-10 18:44:40 +0800

    【改为单节点Registry】简化配置以便于调试

--- service1/src/main/java/com/springcloudneflixeureka/server1/Service1Application.java
+++ service1/src/main/java/com/springcloudneflixeureka/server1/Service1Application.java
@@ -3,6 +3,7 @@ package com.springcloudneflixeureka.server1;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.InstanceInfoFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
