a4cc8d0701d30e533acdbc51888fa9e99bf33520
zpfly <zhao.pengfei@huadongmedia.com>
2019-06-26 14:53:26 +0800

    避免重复注册bean

--- consumer1/src/main/java/com/springcloudneflixeureka/consumer1/Consumer1Application.java
+++ consumer1/src/main/java/com/springcloudneflixeureka/consumer1/Consumer1Application.java
@@ -5,12 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@RestController
 @EnableLBRestTemplate
 public class Consumer1Application {
 
@@ -18,7 +16,7 @@ public class Consumer1Application {
     private RestTemplate restTemplate;
 
     @RequestMapping(value = "/ribbon-consumer")
-    public String helloConsumer(){
+    public String helloConsumer() {
         return restTemplate.getForEntity("http://service1/hello", String.class).getBody();
     }
 
--- service1/src/main/java/com/springcloudneflixeureka/server1/Service1Application.java
+++ service1/src/main/java/com/springcloudneflixeureka/server1/Service1Application.java
@@ -3,25 +3,19 @@ package com.springcloudneflixeureka.server1;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.InstanceInfoFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@RestController
 @Slf4j
 public class Service1Application {
 
     @RequestMapping("/hello")
-    public String helloController(){
+    public String helloController() {
         log.info("hello service instance get consumed");
         return "hello!";
     }
-
-    InstanceInfoFactory infoFactory = null;
 
     public static void main(String[] args) {
         SpringApplication.run(Service1Application.class, args);
