2532c020e2e8d80fa3e94ce1fa86e6dac465bd5a
zpfly <zhao.pengfei@huadongmedia.com>
2019-08-15 13:36:57 +0800

    【config-client】加密相关配置

---
+++ config-server/src/main/java/com/springcloud/configserver/WebSecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.springcloud.configserver;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zpf
+ * @date 2019/8/15 13:27
+ */
+@Component
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .anyRequest().authenticated().and()
+                .httpBasic();
+    }
+}
